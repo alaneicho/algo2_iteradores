7ddc64761ba3c8d7cc1014aa636f41935a6b604e
alaneicho <93944293+alaneicho@users.noreply.github.com>
2021-11-18 15:37:49 -0300

    Ej 3 correcto, test pasan

--- src/algoritmos.hpp
+++ src/algoritmos.hpp
@@ -36,7 +36,40 @@ typename Contenedor::value_type promedio(const Contenedor& c){
         ++iterator;
     }
     return suma/cantidad;
+}
+
+//Ej 3
+// Implementar las funciones análogas a los ejercicios 1 y 2 pero esta vez recibiendo iteradores:
+template<class Iterator>
+typename Iterator::value_type minimoIter(const Iterator& desde, const Iterator& hasta){
+    Iterator iterator = desde;
+    typename Iterator::value_type minimo = *iterator;
+    ++iterator;
+    while (iterator != hasta){
+        if (*iterator < minimo){
+            minimo = *iterator;
+        }
+        ++iterator;
+    }
+    return minimo;
 };
+
+
+template<class Iterator>
+typename Iterator::value_type promedioIter(const Iterator& desde, const Iterator& hasta){
+
+    Iterator iterator = desde;
+    typename Iterator::value_type suma = *iterator;
+    int cantidad = 1;
+    ++iterator;
+    while (iterator != hasta){
+        suma = suma + *iterator;
+        cantidad++;
+        ++iterator;
+    }
+    return suma/cantidad;
+}
+
 
 
 #endif //ALGO2_LABO_CLASE5_ALGORITMOS_H
